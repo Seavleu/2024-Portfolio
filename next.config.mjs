cb6ceb319061d100865c02ad0876db4f7f874030
Seavmey <90964875+seavmey1002@users.noreply.github.com>
2024-02-02 23:22:06 +1300

    Config Github Actions (setup-node and publish)

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    basePath: "/my-portfolio",
+    output: "export",
+    reactStrictMode: true,
+};
 
 export default nextConfig;
 
